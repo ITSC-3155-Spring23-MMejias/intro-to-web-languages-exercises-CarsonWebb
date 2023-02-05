3d20249292a219adf85192885d6e1123f78026e7
CarsonWebb <122924322+CarsonWebb@users.noreply.github.com>
2023-02-05 14:51:30 -0500

    Delete exercise_06.py

--- exercise_06.py
+++
@@ -1,9 +0,0 @@
-row=int(input('Enter a row number from 1 to 5:'))
-col=int(input('Enter a col number form 1 to 5:'))
-for i in range(5):
-    for j in range(5):
-        if(i+1==row)and(j+1==col):
-            print('1',end=" ")
-        else:
-            print('0',end=" ")
-    print('')
